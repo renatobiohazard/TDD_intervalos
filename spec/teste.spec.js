0650288dda50198ef6ec928e7853fe099a87299b
Renato Roberto <rrenato.antunes@gmail.com>
2017-08-01 01:02:28 -0300

    adicionando caso para o terceiro teste, verificar se ´e numero

--- spec/teste.spec.js
+++ spec/teste.spec.js
@@ -5,16 +5,20 @@ describe("Falha no teste pois", function() {
 
   var lista0 = [];
   var lista1 = [1,2,3,6,7];
+  var lista2 = [1,'A',3,4];
 
   // PRIMEIRO CASO
   it("a lista é de nula.", function() {
     expect(false).toBe(app.ehNula(lista1));
   });
-  
+  // SEGUNDO CASO
   it("a lista nao é de nula.", function() {
     expect(true).toBe(app.ehNula(lista0));
   });
   
-
+// TERCEIRO CASO
+  it("a lista não é de números.", function() {
+    expect(true).toBe(app.ehNumero(lista2));
+  });
 
 });
